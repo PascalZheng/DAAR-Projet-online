a93f5fc1b06c0da35fddc64a2105e0dbdaea70f6
Asparodia <thiernobah91860@yahoo.fr>
2020-02-07 15:31:39 +0100

    mots + livres dans lequel il apparait le plus

--- Projet online/src/scripts/ScriptMotOccurenceLivre.java
+++ Projet online/src/scripts/ScriptMotOccurenceLivre.java
@@ -1,10 +1,14 @@
 package scripts;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.stream.Stream;
 
 import algorithme.Livre;
@@ -12,26 +16,69 @@ import algorithme.Livre;
 
 public class ScriptMotOccurenceLivre {
 	
+	static class Couple{
+		private double occu;
+		private String livre;
+		
+		public Couple(String m, double occu) {
+			this.livre = m;
+			this.occu = occu;
+		}
+		
+		@Override
+	    public boolean equals(Object o) { 
+	        if (o == this) { 
+	            return true; 
+	        } 
+	        if (!(o instanceof Couple)) { 
+	            return false; 
+	        } 
+	        Couple c = (Couple) o; 
+	        return c.livre.equals(this.livre); 
+	    } 
+	}
+	
 	public static void main(String[] args) throws IOException {
+		String folderPretraiter = "src/livres_pretraiter";
 		
-
-		String fileSourcesVrac = "/Vrac/livres";
-		
-		String folder = "livres";
-		String folderPretraiter = "livres_pretraiter";
-
-		ArrayList<String> files = new ArrayList<>();
-		ArrayList<String> files_pretraiter = new ArrayList<>();
+		Map<String, Couple> allMots = new HashMap<>();
 
 
-		try (Stream<Path> paths = Files.walk(Paths.get(fileSourcesVrac))) {
+		try (Stream<Path> paths = Files.walk(Paths.get(folderPretraiter))) {
 			paths.filter(Files::isRegularFile).parallel().forEach(f -> {
-				files.add(f.toString());
-				files_pretraiter.add(f.toString().replace(folder, folderPretraiter));
+				try {
+					Files.newBufferedReader(Paths.get(f.toString())).lines().forEach(line -> {
+						String[] read = line.split(" ");
+						Couple c = new Couple(f.toString().split("\\\\")[2], Double.valueOf(read[1]) );
+						
+						if(allMots.keySet().contains(read[0])) {
+							if(allMots.get(read[0]).occu < c.occu) {
+								allMots.put(read[0], c);
+							}
+						}else {
+							allMots.put(read[0], c);
+						}
+						
+					});
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
 			});
 		}
-
-		Livre.occurences(files, folder, folderPretraiter);
+		
+		try {
+			BufferedWriter writer2 = new BufferedWriter(new FileWriter("src/centrality/allWords.txt"));
+			String r = "";
+			for (String s : allMots.keySet()) {
+				r += s + " " + allMots.get(s).livre;
+				r += "\n";
+				writer2.write(r);
+				r = "";
+			}
+			writer2.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 
 }
